2b0fb9614dab1dfc957edfa30622754be8b0ff39
aquiwer <70368630+aquiwer@users.noreply.github.com>
2021-11-23 19:00:41 +0100

    Changed registration and login forms II

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import discount from "../../assets/icons/discount.svg";
 import delivery from "../../assets/icons/delivery.svg";
 import support from "../../assets/icons/support.svg";
@@ -17,14 +17,43 @@ export const Register: FC = () => {
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [uniqCode, setUniqCode] = useState("");
-    const [enableUniqCodeField, setEnableUniqCodeField] = useState(false);
+    // const [enableUniqCodeField, setEnableUniqCodeField] = useState(false);
     const isUniqCode = useTypedSelector(state => state.userReducer.isUniqCode);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
 
-    const dispatch = useDispatch();
-    const isValidEmail = (email: string) => {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
+    function openModal() {
+        setIsOpen(true);
     }
+
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    useEffect(() => {
+        if (isUniqCode) {
+            closeModal();
+            dispatch(userRegisterThunk({login, username, email, password}))
+        }
+    }, [isUniqCode])
+
+    const customStyles = {
+        content: {
+            backgroundColor: "#0A0A0A",
+            borderRadius: "10px",
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            display: "grid",
+            gap: "40px",
+            color: "#fff"
+        },
+    };
+    const dispatch = useDispatch();
+
     const checkUniqCodes = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault()
         if (uniqCode.length === 6) {
@@ -33,20 +62,38 @@ export const Register: FC = () => {
     }
     const sendUniqCode = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
-        setEnableUniqCodeField(true);
-        isValidEmail(email)
-        if (email.trim().length) {
+        if (email.trim().length && username.trim().length && password.trim().length && login.trim().length) {
+            setIsOpen(true)
             dispatch(userSendUniqCodeThunk(email));
         }
     }
     const registerUser = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
-        dispatch(userRegisterThunk({login, username, email, password}))
+
     }
+
+
     // @ts-ignore
     return (
         <article className="registerPage-wrapper">
-
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <p>We sent to your mail special code, please copy and paste it.</p>
+                <fieldset className="form-field-wrapper">
+                    <label className='form-field-label' htmlFor="login-field">
+                        Uniq Code
+                    </label>
+                    <input placeholder="XXX-XXX" onChange={(e) => setUniqCode(e.currentTarget.value)}
+                           className='form-field' type="text" id="code-field"/>
+                </fieldset>
+                <fieldset className="form-field-wrapper">
+                    <button onClick={(e) => checkUniqCodes(e)} className='form-field-button'>Register</button>
+                </fieldset>
+            </Modal>
             <form className='form-wrapper'>
                 <fieldset className="form-field-wrapper">
                     <label className='form-field-label' htmlFor="login-field">
@@ -56,29 +103,35 @@ export const Register: FC = () => {
                            className='form-field' type="text" id="login-field"/>
                 </fieldset>
                 <fieldset className="form-field-wrapper">
-                    <label className='form-field-label' htmlFor="login-field">
+                    <label className='form-field-label' htmlFor="username-field">
                         Username
                     </label>
                     <input placeholder="Your username" onChange={(e) => setUsername(e.currentTarget.value)}
-                           className='form-field' type="text" id="login-field"/>
+                           className='form-field' type="text" id="username-field"/>
                 </fieldset>
                 <fieldset className="form-field-wrapper">
-                    <label className='form-field-label' htmlFor="login-field">
+                    <label className='form-field-label' htmlFor="email-field">
                         Email
                     </label>
                     <input required={true} onChange={(e) => setEmail(e.currentTarget.value)} placeholder="Your email"
-                           className='form-field' type="text" id="login-field"/>
+                           className='form-field' type="text" id="email-field"/>
                 </fieldset>
                 <fieldset className="form-field-wrapper">
-                    <label className='form-field-label' htmlFor="login-field">
+                    <label className='form-field-label' htmlFor="password-field">
                         Password
                     </label>
                     <input autoComplete="new-password" onChange={(e) => setPassword(e.currentTarget.value)}
-                           placeholder="Your password" className='form-field' type="password" id="login-field"/>
+                           placeholder="Your password" className='form-field' type="password" id="password-field"/>
                 </fieldset>
-                <fieldset className="form-field-wrapper">
-                    <button onClick={(e) => sendUniqCode(e)} className='form-field-button'>Register</button>
-                </fieldset>
+                {
+                    isUniqCode ? <fieldset className="form-field-wrapper">
+                            <button onClick={(e) => registerUser(e)} className='form-field-button'>Register</button>
+                        </fieldset> :
+                        <fieldset className="form-field-wrapper">
+                            <button onClick={(e) => sendUniqCode(e)} className='form-field-button'>Accept</button>
+                        </fieldset>
+                }
+
                 <fieldset className="form-field-wrapper">
                     <NavLink to={'/login'}>I have the account</NavLink>
                 </fieldset>
